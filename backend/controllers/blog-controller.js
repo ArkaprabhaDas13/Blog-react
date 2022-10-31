2b2c03a1c5b00590c75564bf231be9da43ad353f
ArkaprabhaDas13 <84220013+ArkaprabhaDas13@users.noreply.github.com>
2022-10-31 10:35:43 +0530

    fourth commit

--- backend/controllers/blog-controller.js
+++ backend/controllers/blog-controller.js
@@ -33,3 +33,32 @@ export const addBlog = async (req,res,next)=>{
     }
     return res.status(200).json({blog})
 }
+
+export const updateBlog = async (req,res,next)=>{
+
+    const {title, description} = req.body;
+    
+    const blogId = req.params.id;
+
+    let blog;
+
+    try{
+        blog = await Blog.findByIdAndUpdate(blogId, 
+            {
+                title,
+                description
+            })
+    }
+    catch(err)
+    {
+        return console.log(err);
+    }
+
+    if(!blog)
+    {
+        return res.status(500).json({message:"Unable to update the blog!"});
+    }
+
+    return res.status(200).json({message:"Successfully updated the blog"});
+
+};
--- backend/routes/blog-routes.js
+++ backend/routes/blog-routes.js
@@ -1,7 +1,9 @@
 import  express  from "express";
-import {getAllBlogs,addBlog} from "../controllers/blog-controller"
+import {getAllBlogs,addBlog,updateBlog} from "../controllers/blog-controller"
 const blogRouter = express.Router();
 
 blogRouter.get("/",getAllBlogs);    //showing all blogs
 blogRouter.post("/add",addBlog);    //adding blogs
+blogRouter.put("/update/:id", updateBlog);
+
 export default blogRouter;
